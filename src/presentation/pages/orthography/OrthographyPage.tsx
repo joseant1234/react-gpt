a9d5b44d3e1370d77e9e8133e360750ef89a7462
Jose Tafur <joseantonioumeres@gmail.com>
2024-01-05 14:16:32 -0500

    Added chat screen

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,8 +1,14 @@
+import { GptMessage, MyMessage } from "../../components"
 
 export const OrthographyPage = () => {
   return (
-    <div>
-      Ortografia
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          <GptMessage text="Hola, puedes escribir tu texto en español, y te ayudo con las correcciones" />
+          <MyMessage text="Hola" />
+        </div>
+      </div>
     </div>
   )
 }
