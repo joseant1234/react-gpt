a5dea1c25f699d13fd318457b2bc2364ffd4a07c
Jose Tafur <joseantonioumeres@gmail.com>
2024-02-28 21:10:27 -0500

    Added image generation and image variation pages

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,5 +1,7 @@
 export * from './chat-bubbles/GptMessage';
 export * from './chat-bubbles/GptMessageAudio';
+export * from './chat-bubbles/GptMessageImage'
+export * from './chat-bubbles/GptMessageSelectableImage';
 export * from './chat-bubbles/GptOrthopgraphyMessage';
 export * from './chat-bubbles/MyMessage';
 export * from './chat-input-boxes/TextMessageBox';
